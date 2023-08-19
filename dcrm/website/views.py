9b2753b859bf0c2a7d9f720d864eda2227ad3f22
GitHudsonLima <hudsonoliveiral@outlook.com>
2023-08-19 00:46:22 -0400

    Login Users

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html',{})
+    # Check to see if logging in
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again")
+            return redirect('home')
+        
+    else:
+        return render(request, 'home.html',{})
+
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, "You Have Been Logged Out")
+    return redirect('home')
+
