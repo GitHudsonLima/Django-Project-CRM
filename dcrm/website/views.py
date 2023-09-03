bc83f47252ab6170009bf88ca4c8fe65499f1ced
GitHudsonLima <hudsonoliveiral@outlook.com>
2023-09-03 09:20:29 -0400

    registration

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home(request):
     # Check to see if logging in
@@ -28,5 +29,13 @@ def logout_user(request):
     return redirect('home')
 
 def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            
+
+
+
     return render(request, 'register.html',{})
 
