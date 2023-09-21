9b33ca5a6855988de1ae799520bf8501dd225a3e
GitHudsonLima <hudsonoliveiral@outlook.com>
2023-09-20 23:45:16 -0400

    View records

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpForm
+from .models import Record
 
 def home(request):
+    records = Record.objects.all()
+
     # Check to see if logging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -19,7 +22,7 @@ def home(request):
             return redirect('home')
         
     else:
-        return render(request, 'home.html',{})
+        return render(request, 'home.html',{'records':records})
 
 
 
