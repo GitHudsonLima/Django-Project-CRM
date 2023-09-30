fa4458bacb2c004a48c120c756a57aa7346770a1
GitHudsonLima <hudsonoliveiral@outlook.com>
2023-09-30 01:27:20 -0400

    Final project

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -15,7 +15,7 @@ def home(request):
         user = authenticate(request, username = username, password = password)
         if user is not None:
             login(request, user)
-            messages.success(request, "You Have Been Logged In!")
+            #messages.success(request, "You Have Been Logged In!")
             return redirect('home')
         else:
             messages.success(request, "There Was An Error Logging In, Please Try Again")
@@ -28,7 +28,7 @@ def home(request):
 
 def logout_user(request):
     logout(request)
-    messages.success(request, "You Have Been Logged Out")
+    #messages.success(request, "You Have Been Logged Out")
     return redirect('home')
 
 def register_user(request):
@@ -82,3 +82,19 @@ def add_record(request):
     else:
         messages.success(request, "You Must Be Logged In To View That Page...")
         return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id=pk)
+        form = AddRecordForm(request.POST or None, instance=current_record)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Record Has Been Updated!")
+            return redirect('home')
+        return render(request, 'update_record.html', {'form':form})
+    else:
+        messages.success(request, "You Must Be Logged In To View That Page...")
+        return redirect('home')
+
+
